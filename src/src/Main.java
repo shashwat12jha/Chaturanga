bda299e49235af87d48527f1b174b64ebf1afdfa
Shashwat Kumar Jha <shashwat1227@gmail.com>
2026-07-01 10:05:10 +0530

    Creating the ChessBoard

---
+++ src/src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.getContentPane().setBackground(Color.black);
+        frame.setLayout(new GridBagLayout());
+        frame.setMinimumSize(new Dimension(1000,1000));
+        frame.setLocationRelativeTo(null);
+        Board board= new Board();
+        frame.add(board);
+        frame.setVisible(true);
+    }
+}
